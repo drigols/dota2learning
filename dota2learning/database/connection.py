70cf504067419536835c904f47b81cc70e26c734
drigols <drigols.creative@gmail.com>
2022-07-12 20:47:11 -0300

    update Connection class

--- dota2learning/database/connection.py
+++ dota2learning/database/connection.py
@@ -4,22 +4,40 @@
 from __future__ import annotations
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# Database settings.
-username: str = "root"
-password: str = "toor"
-hostname: str = "localhost"
-database: str = "dota2learning-db"
+class Connection:
 
-DATABASE_URL = f"mysql+pymysql://{username}:{password}@{hostname}/{database}"
+    user: str = "root"
+    passwd: str = "toor"
+    host: str = "localhost"
+    dbName: str = "dota2learning-db"
+    port: str = "3306"
+    DATABASE_URL = f"mysql+pymysql://{user}:{passwd}@{host}:{port}/{dbName}"
 
-# Database Connection (engine)
-engine = create_engine(
-    DATABASE_URL,
-    echo=True,
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+    def __init__(self) -> None:
+        self.connection_is_active = False
+        self.engine = None
+
+    def get_engine_connection(self):
+        if self.connection_is_active is False:
+            try:
+                self.engine = create_engine(
+                    self.DATABASE_URL,
+                )
+                return self.engine
+            except Exception as error:
+                print(error)
+        return self.engine
+
+    def get_session(self, engine):
+        try:
+            Session = sessionmaker(
+                autocommit=False, autoflush=False, bind=engine
+            )
+            session = Session()
+            return session
+        except Exception as error:
+            print(error)
+            return None
