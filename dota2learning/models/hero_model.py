792311072ba28dbec1e225ed0b277bb6e11a330c
drigols <drigols.creative@gmail.com>
2022-07-12 20:46:09 -0300

    Add nullable=False for table NOT NULL constraints

--- dota2learning/models/hero_model.py
+++ dota2learning/models/hero_model.py
@@ -7,33 +7,34 @@ from sqlalchemy import Column
 from sqlalchemy import Float
 from sqlalchemy import Integer
 from sqlalchemy import VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
 
-from dota2learning.database.connection import Base
+Base = declarative_base()
 
 
 class HeroModel(Base):
     __tablename__ = "hero"
     id = Column(Integer, primary_key=True)
-    name = Column(VARCHAR(50))
-    localized_name = Column(VARCHAR(50))
-    primary_attr = Column(VARCHAR(50))
-    attack_type = Column(VARCHAR(50))
-    img = Column(VARCHAR(100))
-    icon = Column(VARCHAR(100))
-    base_health = Column(Float)
-    base_health_regen = Column(Float)
-    base_mana = Column(Float)
-    base_mana_regen = Column(Float)
-    base_armor = Column(Float)
-    base_attack_min = Column(Float)
-    base_attack_max = Column(Float)
-    base_str = Column(Float)
-    base_agi = Column(Float)
-    base_int = Column(Float)
-    str_gain = Column(Float)
-    agi_gain = Column(Float)
-    int_gain = Column(Float)
-    attack_range = Column(Float)
-    projectile_speed = Column(Float)
-    move_speed = Column(Float)
-    legs = Column(Integer)
+    name = Column(VARCHAR(50), nullable=False)
+    localized_name = Column(VARCHAR(50), nullable=False)
+    primary_attr = Column(VARCHAR(50), nullable=False)
+    attack_type = Column(VARCHAR(50), nullable=False)
+    img = Column(VARCHAR(100), nullable=False)
+    icon = Column(VARCHAR(100), nullable=False)
+    base_health = Column(Float, nullable=False)
+    base_health_regen = Column(Float, nullable=False)
+    base_mana = Column(Float, nullable=False)
+    base_mana_regen = Column(Float, nullable=False)
+    base_armor = Column(Float, nullable=False)
+    base_attack_min = Column(Float, nullable=False)
+    base_attack_max = Column(Float, nullable=False)
+    base_str = Column(Float, nullable=False)
+    base_agi = Column(Float, nullable=False)
+    base_int = Column(Float, nullable=False)
+    str_gain = Column(Float, nullable=False)
+    agi_gain = Column(Float, nullable=False)
+    int_gain = Column(Float, nullable=False)
+    attack_range = Column(Float, nullable=False)
+    projectile_speed = Column(Float, nullable=False)
+    move_speed = Column(Float, nullable=False)
+    legs = Column(Integer, nullable=False)
