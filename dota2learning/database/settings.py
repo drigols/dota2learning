e51d091bfdbcd699a89fa452f4f3c60a3ec9cefd
drigols <drigols.creative@gmail.com>
2022-07-13 09:41:55 -0300

    Add Database default settings

---
+++ dota2learning/database/settings.py
@@ -0,0 +1,11 @@
+# Authors: Rodrigo Leite <drigols.creative@gmail.com>
+#
+# License: MIT
+from __future__ import annotations
+
+# Database settings.
+db_settings = {
+    "username": "root",
+    "password": "toor",
+    "database": "dota2learning-db",
+}
