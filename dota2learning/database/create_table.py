657432224ce523e7e985e6a64c6a1cf2348e0398
drigols <drigols.creative@gmail.com>
2022-07-13 05:02:08 -0300

    Update success letter

--- dota2learning/database/create_table.py
+++ dota2learning/database/create_table.py
@@ -8,10 +8,7 @@ from sqlalchemy.engine.reflection import Inspector
 from dota2learning.database.connection import Connection
 
 
-def create_table_if_not_exists(model) -> bool:
-
-    # Variable to control if table exists or not.
-    status = False
+def create_table_if_not_exists(model):
 
     # Database Connection.
     connEngine = Connection().get_engine_connection()
@@ -19,9 +16,6 @@ def create_table_if_not_exists(model) -> bool:
     # Check if table exists.
     if not Inspector(connEngine).has_table(model.__tablename__):
         model.metadata.create_all(connEngine)
-        print("table created successfully!")
-        status = True
-        return status
+        print("Table created successfully!")
     else:
         print("Your table already exists!")
-        return status
