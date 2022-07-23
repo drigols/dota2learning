917fd762b78259e6520ea0965b338559bc1db4f4
drigols <drigols.creative@gmail.com>
2022-07-23 15:13:03 -0300

    Add get_engine_connection function Create a function to get an Engine connection reference.

--- dota2learning/database/connection.py
+++ dota2learning/database/connection.py
@@ -4,16 +4,39 @@
 from __future__ import annotations
 
 from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.pool import NullPool
 
 
 def get_engine_connection(
-    username, password, database, host="localhost", port="3306"
+    username: str,
+    password: str,
+    database: str,
+    host: str = "localhost",
+    port: int = 3306,
 ):
+    """Function to get an Enginer connection reference.
+
+    Args:
+        username (str): Database username to connect.
+        password (str): Password for your username connection.
+        database (str): Database name you want to connect.
+        host (str, optional): Hostname. Defaults to "localhost".
+        port (int, optional): Service port. Defaults to "3306".
+
+    Returns:
+        <class 'sqlalchemy.engine.base.Connection'>:
+        The return is an Engine connection reference to the Database.
+    """
     try:
         engine = create_engine(
             f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}",
             echo=False,
+            poolclass=NullPool,
         )
+        connection = engine.connect()
+    except OperationalError:
+        print("OperationalError: Check your dialect URL or database service.")
+    else:
+        connection.close()
         return engine
-    except Exception as error:
-        print(error)
--- dota2learning/test_drive.py
+++ dota2learning/test_drive.py
@@ -3,33 +3,21 @@
 # License: MIT
 from __future__ import annotations
 
-from dota2learning.api.opendota import get_heroes_stats
 from dota2learning.database.connection import get_engine_connection
-from dota2learning.database.create_table import create_table_if_not_exists
-from dota2learning.database.inserts import insert_into_table
-from dota2learning.database.settings import db_settings
-from dota2learning.models.hero_model import HeroModel
+from dota2learning.settings import db_settings
 
 
 if __name__ == "__main__":
 
-    # Connection Testing.
-    connEngine = get_engine_connection(**db_settings)
-    print("\nConnection Testing:")
-    print("Variable result:", connEngine)
-    print("Type Result:", type(connEngine))
-    print("Bool result:", bool(connEngine))
-
-    # Create Table Testing.
-    print("\nCreate Table Testing:")
-    cTable = create_table_if_not_exists(model=HeroModel)
-    print("Variable result:", cTable)
-    print("Type Result:", type(cTable))
-    print("Bool result:", bool(cTable))
-
-    # Inser into Table Testing.
-    print("\nInsert into Table Testing:")
-    insertIntoHero = insert_into_table(HeroModel, get_heroes_stats())
-    print("Variable result:", insertIntoHero)
-    print("Type Result:", type(insertIntoHero))
-    print("Bool result:", bool(insertIntoHero))
+    # Testing connection.
+    engine = get_engine_connection(
+        username="root",
+        password="toor",
+        database="dota2learning-db",
+        host="localhost",
+        port=3306,
+    )
+    print("Testing connection:")
+    print("Variable result:", engine)
+    print("Type Result:", type(engine))
+    print("Bool result:", bool(engine))
