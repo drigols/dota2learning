8dbd5d08499462efebc6d2508c310161c6c0d707
drigols <drigols.creative@gmail.com>
2022-07-24 21:46:44 -0300

    Add create tables function

---
+++ dota2learning/database/create_table.py
@@ -0,0 +1,25 @@
+# Authors: Rodrigo Leite <drigols.creative@gmail.com>
+#
+# License: MIT
+from __future__ import annotations
+
+from sqlalchemy.engine.reflection import Inspector
+
+from dota2learning.database.connection import get_engine_connection
+from dota2learning.settings import db_settings
+
+
+def create_table_if_not_exists(model):
+
+    # Get Engine connection.
+    engine = get_engine_connection(**db_settings)
+
+    try:
+        # Check if table exists.
+        if Inspector(engine).has_table(model.__tablename__):
+            print(f'Table "{model.__tablename__}" already exists!')
+        else:
+            model.metadata.create_all(engine)
+            print(f'Table "{model.__tablename__}" created successfully!')
+    except Exception as error:
+        print(error)
--- dota2learning/test_drive.py
+++ dota2learning/test_drive.py
@@ -4,20 +4,27 @@
 from __future__ import annotations
 
 from dota2learning.database.connection import get_engine_connection
+from dota2learning.database.create_table import create_table_if_not_exists
+from dota2learning.models.hero_model import HeroModel
 from dota2learning.settings import db_settings
 
 
 if __name__ == "__main__":
 
     # Testing connection.
-    engine = get_engine_connection(
-        username="root",
-        password="toor",
-        database="dota2learning-db",
-        host="localhost",
-        port=3306,
-    )
-    print("Testing connection:")
-    print("Variable result:", engine)
-    print("Type Result:", type(engine))
-    print("Bool result:", bool(engine))
+    # engine = get_engine_connection(
+    #    username="root",
+    #    password="toor",
+    #    database="dota2learning-db",
+    #    host="localhost",
+    #    port=3306,
+    # )
+    # print("Testing connection:")
+    # print("Returned value:", engine)
+    # print("Returned value type:", type(engine))
+
+    # Testing connection.
+    ctifnoexResult = create_table_if_not_exists(HeroModel)
+    print("\nTesting create table if not exists:")
+    print("Returned value:", ctifnoexResult)
+    print("Returned value type:", type(ctifnoexResult))
